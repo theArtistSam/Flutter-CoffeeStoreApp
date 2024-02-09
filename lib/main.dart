e63d0ea5638cc1180e90d51bd7387ba2bc04f211
Abdullah Mehdi <theartistsam47@gmail.com>
2024-02-10 03:17:27 +0500

    App Completed

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,15 @@
 import 'package:coffee_app/pages/start/start_page.dart';
+import 'package:flutter/foundation.dart';
 import 'package:flutter/services.dart' as services;
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:google_fonts/google_fonts.dart';
 
 void main() {
+  LicenseRegistry.addLicense(() async* {
+    final license = await rootBundle.loadString('google_fonts/OFL.txt');
+    yield LicenseEntryWithLineBreaks(['google_fonts'], license);
+  });
   runApp(const MyApp());
 }
 
